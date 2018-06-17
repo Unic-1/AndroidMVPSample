6a92ea6fc81a2733ee16567f6f14b498e3d2126f
Unic-1 <satyam123.sr@gmail.com>
2018-06-17 11:16:34 +0530

    implementing data binding

--- app/src/main/java/com/example/unic_1/mvptest/view/MainActivity.java
+++ app/src/main/java/com/example/unic_1/mvptest/view/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.unic_1.mvptest.view;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.example.unic_1.mvptest.R;
 import com.example.unic_1.mvptest.contractor.MainContract;
+import com.example.unic_1.mvptest.databinding.ActivityMainBinding;
 import com.example.unic_1.mvptest.presenter.MainPresenter;
 
 /**
@@ -13,14 +15,15 @@ import com.example.unic_1.mvptest.presenter.MainPresenter;
  */
 public class MainActivity extends AppCompatActivity implements MainContract.MainView {
 
-    MainContract.MainPresenter mPresenter;
+    MainPresenter mPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+//        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         mPresenter = new MainPresenter(this);
+        binding.setPresenter(mPresenter);
     }
 
     //////      MainView Methods       ////////
